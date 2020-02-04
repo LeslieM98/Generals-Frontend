175b77323396bc918b68213dc887c2d6493e8859
Leslie <2007732+LeslieM98@users.noreply.github.com>
2020-02-04 23:08:08 +0100

    removed restcalls from battlemap

--- src/components/BattleMap.component.jsx
+++ src/components/BattleMap.component.jsx
@@ -2,15 +2,6 @@ import React from "react";
 import axios from "axios";
 
 const BattleMap = props => {
-  const displayedUnits = props.displayedUnits.map(x =>
-    axios.get(`http://localhost:8080/troop/get?id=${x}`, {
-      body: {
-        value: x
-      }
-    })
-  );
-  displayedUnits.forEach(x => x.then(y => console.log(y)));
-
   return (
     <svg xmlns="http://www.w3.org/2000/svg">
       <g name="BattleMap"></g>
