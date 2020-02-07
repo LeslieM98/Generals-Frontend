a7b627e67f1d0575e89c7ee9a08ea3e0787df4d3
Leslie <2007732+LeslieM98@users.noreply.github.com>
2020-02-07 19:23:23 +0100

    cleaned up UnitStatus

--- src/components/BaseUnit.component.jsx
+++ src/components/BaseUnit.component.jsx
@@ -5,22 +5,16 @@ const UNIT_HEIGHT = 50;
 const UNIT_WIDTH = 100;
 const BAR_HEIGHT = 10;
 
-const UnitStatus = props => {
-  console.log(props.healthData);
-  if (props.healthData !== null) {
-    console.log(props.healthData.current);
-    console.log(props.healthData.maximum);
-  }
-  const hp =
-    props.healthData && props.healthData.current / props.healthData.maximum;
-  console.log(hp);
+const UnitStatus = ({ healthData }) => {
+  console.log("healthData:", healthData);
+  const HP = healthData && healthData.current / healthData.maximum;
 
   return (
     <svg xmlns="http://www.w3.org/2000/svg">
       <g name="UnitStatus">
         <rect width={UNIT_WIDTH} height={BAR_HEIGHT} y={UNIT_HEIGHT}></rect>
         <rect
-          width={UNIT_WIDTH * hp}
+          width={UNIT_WIDTH * HP}
           height={BAR_HEIGHT}
           y={UNIT_HEIGHT}
           fill="green"
