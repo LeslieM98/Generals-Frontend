d88d267846510617bbcdbe260425e02587f3f1c8
Leslie <2007732+LeslieM98@users.noreply.github.com>
2020-04-18 15:40:22 +0200

    changed project to TS

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import BaseUnit from "./components/BaseUnit.component";
-import changeOrder from "./renderordermanipulator";
 
 const App = () => {
   return (
     <div>
-      <BaseUnit id="1" />
       Generals-Frontend WIP
     </div>
   );
@@ -14,4 +11,3 @@ const App = () => {
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-changeOrder(["Ranges"]);
--- src/renderordermanipulator.js
+++
@@ -1,7 +0,0 @@
-function changeOrder(classOrder, targetParent) {
-  const elements = {};
-  classOrder.forEach(x => (elements[x] = document.getElementsByClassName(x)));
-  console.log("ORDER", elements);
-}
-
-export default changeOrder;
